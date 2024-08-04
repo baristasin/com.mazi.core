27f656a5cbfd265315578ca043325a969c84ed4b
Barış Taşin <tasinbaris95@gmail.com>
2024-08-04 20:03:18 +0300

    IronSource updated

--- Runtime/Mediations/IronSourceMediation.cs
+++ Runtime/Mediations/IronSourceMediation.cs
@@ -14,90 +14,62 @@ namespace Mazi.Mediations
 
             IronSource.Agent.init("135a6c241", IronSourceAdUnits.REWARDED_VIDEO, IronSourceAdUnits.INTERSTITIAL, IronSourceAdUnits.BANNER);
 
-            IronSource.Agent.validateIntegration();
-
-            IronSourceEvents.onRewardedVideoAdOpenedEvent += RewardedVideoAdOpenedEvent;
-            IronSourceEvents.onRewardedVideoAdClickedEvent += RewardedVideoAdClickedEvent;
-            IronSourceEvents.onRewardedVideoAdClosedEvent += RewardedVideoAdClosedEvent;
-            IronSourceEvents.onRewardedVideoAvailabilityChangedEvent += RewardedVideoAvailabilityChangedEvent;
-            IronSourceEvents.onRewardedVideoAdStartedEvent += RewardedVideoAdStartedEvent;
-            IronSourceEvents.onRewardedVideoAdEndedEvent += RewardedVideoAdEndedEvent;
-            IronSourceEvents.onRewardedVideoAdRewardedEvent += RewardedVideoAdRewardedEvent;
-            IronSourceEvents.onRewardedVideoAdShowFailedEvent += RewardedVideoAdShowFailedEvent;
+            //Add AdInfo Rewarded Video Events
+            IronSourceRewardedVideoEvents.onAdOpenedEvent += RewardedVideoOnAdOpenedEvent;
+            IronSourceRewardedVideoEvents.onAdClosedEvent += RewardedVideoOnAdClosedEvent;
+            IronSourceRewardedVideoEvents.onAdAvailableEvent += RewardedVideoOnAdAvailable;
+            IronSourceRewardedVideoEvents.onAdUnavailableEvent += RewardedVideoOnAdUnavailable;
+            IronSourceRewardedVideoEvents.onAdShowFailedEvent += RewardedVideoOnAdShowFailedEvent;
+            IronSourceRewardedVideoEvents.onAdRewardedEvent += RewardedVideoOnAdRewardedEvent;
+            IronSourceRewardedVideoEvents.onAdClickedEvent += RewardedVideoOnAdClickedEvent;
 
 
-            IronSourceEvents.onInterstitialAdReadyEvent += InterstitialAdReadyEvent;
-            IronSourceEvents.onInterstitialAdLoadFailedEvent += OnInterstitialAdLoadFailedEvent;           
+            IronSource.Agent.validateIntegration();      
         }
 
-        private void OnInterstitialAdLoadFailedEvent(IronSourceError error)
-        {
 
-        }
-
-        private void InterstitialAdReadyEvent()
-        {
-
-        }
-
-        //Invoked when the RewardedVideo ad view has opened.
-        //Your Activity will lose focus. Please avoid performing heavy 
-        //tasks till the video ad will be closed.
-        void RewardedVideoAdOpenedEvent()
+        /************* RewardedVideo AdInfo Delegates *************/
+        // Indicates that there’s an available ad.
+        // The adInfo object includes information about the ad that was loaded successfully
+        // This replaces the RewardedVideoAvailabilityChangedEvent(true) event
+        void RewardedVideoOnAdAvailable(IronSourceAdInfo adInfo)
         {
         }
-        //Invoked when the RewardedVideo ad view is about to be closed.
-        //Your activity will now regain its focus.
-        void RewardedVideoAdClosedEvent()
+        // Indicates that no ads are available to be displayed
+        // This replaces the RewardedVideoAvailabilityChangedEvent(false) event
+        void RewardedVideoOnAdUnavailable()
         {
         }
-        //Invoked when there is a change in the ad availability status.
-        //@param - available - value will change to true when rewarded videos are available. 
-        //You can then show the video by calling showRewardedVideo().
-        //Value will change to false when no videos are available.
-        void RewardedVideoAvailabilityChangedEvent(bool available)
+        // The Rewarded Video ad view has opened. Your activity will loose focus.
+        void RewardedVideoOnAdOpenedEvent(IronSourceAdInfo adInfo)
         {
-            //Change the in-app 'Traffic Driver' state according to availability.
-            bool rewardedVideoAvailability = available;
         }
-
-        //Invoked when the user completed the video and should be rewarded. 
-        //If using server-to-server callbacks you may ignore this events and wait for 
-        // the callback from the  ironSource server.
-        //@param - placement - placement object which contains the reward data
-        void RewardedVideoAdRewardedEvent(IronSourcePlacement placement)
+        // The Rewarded Video ad view is about to be closed. Your activity will regain its focus.
+        void RewardedVideoOnAdClosedEvent(IronSourceAdInfo adInfo)
+        {
+        }
+        // The user completed to watch the video, and should be rewarded.
+        // The placement parameter will include the reward data.
+        // When using server-to-server callbacks, you may ignore this event and wait for the ironSource server callback.
+        void RewardedVideoOnAdRewardedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
         {
             OnRewardedVideoDismissed?.Invoke(true);
         }
-        //Invoked when the Rewarded Video failed to show
-        //@param description - string - contains information about the failure.
-        void RewardedVideoAdShowFailedEvent(IronSourceError error)
+        // The rewarded video ad was failed to show.
+        void RewardedVideoOnAdShowFailedEvent(IronSourceError error, IronSourceAdInfo adInfo)
         {
             OnRewardedVideoDismissed?.Invoke(false);
         }
-
-        // ----------------------------------------------------------------------------------------
-        // Note: the events below are not available for all supported rewarded video ad networks. 
-        // Check which events are available per ad network you choose to include in your build. 
-        // We recommend only using events which register to ALL ad networks you include in your build. 
-        // ----------------------------------------------------------------------------------------
-
-        //Invoked when the video ad starts playing. 
-        void RewardedVideoAdStartedEvent()
-        {
-        }
-        //Invoked when the video ad finishes playing. 
-        void RewardedVideoAdEndedEvent()
-        {
-        }
-        //Invoked when the video ad is clicked. 
-        void RewardedVideoAdClickedEvent(IronSourcePlacement placement)
+        // Invoked when the video ad was clicked.
+        // This callback is not supported by all networks, and we recommend using it only if
+        // it’s supported by all networks you included in your build.
+        void RewardedVideoOnAdClickedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
         {
         }
 
         public void LoadRewarded()
         {
-            IronSource.Agent.loadManualRewardedVideo();
+            IronSource.Agent.loadRewardedVideo();
         }
 
         public void ShowRewarded()
